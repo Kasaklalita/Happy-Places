519b1e557099fde5e61dca9b3c215f9fac628a7f
Veniamin Polienko <kasaklalita@mail.ru>
2022-10-09 13:48:36 +0300

    Addint the HappyPlaceActivity and using dimens string color

--- app/src/main/java/com/kasaklalita/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/kasaklalita/happyplaces/AddHappyPlaceActivity.kt
@@ -2,10 +2,17 @@ package com.kasaklalita.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_happy_place.*
 
 class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
+        setSupportActionBar(tbAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        tbAddPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
